ee2468b5850d769a6e77826de0d1388cb99b2762
jamiecartin <jamie.cartin@icloud.com>
2023-03-14 11:49:05 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jamie Cartin on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
